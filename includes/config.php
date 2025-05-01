b6ca6644d1e32cba97296965f268cd0f5f9436fb
Azriel-Fajar <116171291+Azriel-Fajar@users.noreply.github.com>
2025-05-01 15:31:03 +0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,9 +5,9 @@ date_default_timezone_set('Asia/Jakarta');
 
 // Set folder permission warning
 if (!is_writable(__DIR__ . '/../data')) {
-    @mkdir(__DIR__ . '/../data', 0777, true);
+    @mkdir(__DIR__ . '/data', 0777, true);
 }
 
-if (!is_writable(__DIR__ . '/../data')) {
+if (!is_writable(__DIR__ . '/data')) {
     die('<div class="alert alert-danger">Folder "data" tidak bisa ditulisi. Mohon set permission folder menjadi 0777.</div>');
 }
